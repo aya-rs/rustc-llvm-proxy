67fad50e5a32e5720651d439cd6d35f08c257093
Michal Rostecki <vadorovsky@gmail.com>
2022-12-19 18:57:24 +0100

    path: Add `LLVM_SYS_150_PREFIX` values to possible directories
    Using `LLVM_SYS_150_PREFIX` is comfortable when testing with locally
    built LLVM, it removes the necessity of installing it. This is the same
    enviroment variable that llvm-sys crate uses.
    
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- src/path.rs
+++ src/path.rs
@@ -4,6 +4,11 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use failure::Error;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"^libLLVM.*\.so").unwrap();
+}
 
 pub fn find_lib_path() -> Result<PathBuf, Error> {
     let directories = collect_possible_directories();
@@ -31,6 +36,15 @@ pub fn find_lib_path() -> Result<PathBuf, Error> {
 fn collect_possible_directories() -> Vec<PathBuf> {
     let mut paths = vec![];
     let separator = if cfg!(windows) { ';' } else { ':' };
+
+    if let Ok(build_paths) = env::var("LLVM_SYS_150_PREFIX") {
+        for item in build_paths.split(separator) {
+            let mut possible_path = PathBuf::from(item);
+
+            possible_path.push("lib");
+            paths.push(possible_path);
+        }
+    }
 
     if let Ok(lib_paths) = env::var("LD_LIBRARY_PATH") {
         for item in lib_paths.split(separator) {
@@ -61,9 +75,29 @@ fn find_library_in_directory(directory: &Path) -> Option<PathBuf> {
     match read_dir(directory) {
         Ok(files) => files
             .filter_map(Result::ok)
-            .find(|file| file.file_name().to_string_lossy().starts_with("libLLVM"))
+            .find(|file| RE.is_match(file.file_name().to_string_lossy().as_ref()))
             .map(|file| file.path()),
 
         Err(_) => None,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_llvm_lib_regex() {
+        // Possible names in packages.
+        assert!(RE.is_match("libLLVM.so"));
+        assert!(RE.is_match("libLLVM.so.15"));
+        assert!(RE.is_match("libLLVM.so.15.0"));
+        assert!(RE.is_match("libLLVM.so.15.0.6"));
+        // Possible names in Rust toolchains.
+        assert!(RE.is_match("libLLVM-15-rust-1.66.0-stable.so"));
+        assert!(RE.is_match("libLLVM-15-rust-1.67.0-beta.so"));
+        assert!(RE.is_match("libLLVM-15-rust-1.68.0-nightly.so"));
+        // Name in local build.
+        assert!(RE.is_match("libLLVM-15.so"));
     }
 }
